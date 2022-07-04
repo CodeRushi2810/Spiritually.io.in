5c9f90390e9b87c34c68a8924c0fe6b12a73c957
Rushi <patelrushi2810@gmail.com>
2022-07-04 12:19:00 +0530

    initial commil

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+console.log("Connected Successfully");
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
+
+let preloader = document.getElementById('loading');
+function myFunction() {
+    preloader.style.display = 'none';
+}   
+
