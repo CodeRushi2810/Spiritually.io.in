04fb0281a3d96462c3441050f7084091e14641b1
Rushi Patel <Gautam.Mishra-v@dhentertainment.ae>
2024-11-27 13:34:42 +0530

    Updated JS

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-
-
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
-console.log("Connected Successfully");
 
 if (bar) {
     bar.addEventListener('click', () => {
@@ -16,10 +13,7 @@ if (close) {
     })
 }
 
-
-
 let preloader = document.getElementById('loading');
 function myFunction() {
     preloader.style.display = 'none';
 }   
-
